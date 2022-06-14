96bdb2f709f4156a07a209201dc364792da4e944
Siddharth Misra <misrasiddharth9@gmail.com>
2022-06-14 19:13:40 +0530

    day 2 completed

---
+++ lib/Homepage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Catalog App"),
+      ),
+      body: Center(
+          child: Container(child: const Text('Welcome to 30 days of Flutter'))),
+    );
+  }
+}
